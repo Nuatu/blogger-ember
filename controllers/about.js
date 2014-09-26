83ab937c0de6de66ad1ef6275a35917702a9fe39
Sterling Houghton & Nuatu Tseggai <student@epicodus.com>
2014-09-26 12:04:39 -0700

    create contact template

---
+++ controllers/about.js
@@ -0,0 +1,7 @@
+Blogger.AboutController=Ember.Controller.extend({
+  actions: {
+    showRealName: function() {
+      alert("Dracula is fictional, but was inspired by the 15th-centry Romanian general and Wallachian Prince Vlad III the Impaler.")
+    }
+  }
+});
